d918c55d7ea58ed3a46ede2ef92e0964fd063dfb
Shinkuvichh <112030252+Shinkuvichh@users.noreply.github.com>
2023-12-22 22:31:57 +0300

    Reformat task5.sql

--- task5.sql
+++ task5.sql
@@ -1,19 +1,32 @@
 set search_path = supermarket, public;
 
-insert into supermarket.cards values ('79313402511', 'Оливия', 'ysee@y.ru');
+insert into cards
+values ('79313402511', 'Оливия', 'ysee@y.ru');
 
-select owner_name, owner_email from supermarket.cards where owner_phone = '79313402511';
+select owner_name, owner_email
+from cards
+where owner_phone = '79313402511';
 
-update supermarket.cards set owner_email = 'olivia@y.com' where owner_phone = '79313402511';
+update cards
+set owner_email = 'olivia@y.com'
+where owner_phone = '79313402511';
 
-delete from supermarket.cards where owner_phone = '79313402511';
+delete
+from cards
+where owner_phone = '79313402511';
 
 
+insert into products(product_name, product_price, product_manufacturer)
+values ('Хлеб "Белый" 150 гр', 30, 'Хлебзавод 1');
 
-insert into supermarket.products(product_name, product_price, product_manufacturer) values ('Хлеб "Белый" 150 гр', 30, 'Хлебзавод 1');
+select product_price
+from products
+where product_name = 'Хлеб "Белый" 150 гр';
 
-select product_price from supermarket.products where product_name = 'Хлеб "Белый" 150 гр';
+update products
+set product_price = 40
+where product_name = 'Хлеб "Белый" 150 гр';
 
-update supermarket.products set product_price = 40 where product_name = 'Хлеб "Белый" 150 гр';
-
-delete from supermarket.products where product_name = 'Хлеб "Белый" 150 гр';
+delete
+from products
+where product_name = 'Хлеб "Белый" 150 гр';
